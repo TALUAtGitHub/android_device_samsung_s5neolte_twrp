e42ea46d89c06a3d56661731a6b0dea3ec40a898
TALU <taluemail272@gmail.com>
2021-04-28 20:39:01 +0100

    s5neolte: remove the unused prebuilt dt.img.
    No need to have it lying around with no purpose.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,7 @@
-
 include device/samsung/exynos7580-common/BoardConfigCommon.mk
 
 TARGET_KERNEL_CONFIG := lineageos_s5neolte_defconfig
 
-#BOARD_MKBOOTIMG_ARGS += --dt device/samsung/s5neolte/prebuilt/dt.img --board SYSMAGIC000KU
 BOARD_MKBOOTIMG_ARGS += --board SYSMAGIC000KU
 
 BOARD_KERNEL_SEPARATED_DT := true
